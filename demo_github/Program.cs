19176f7624683383708ae1ca3fa898bf01fc40be
phung minh duong <phungminhduong59@gmail.com>
2024-12-20 21:18:20 +0700

    Add project files.

---
+++ demo_github/Program.cs
@@ -0,0 +1,31 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+
+
+// Tôi sẽ không bao giờ bỏ cuộc, tôi sẽ trở thành một developer giỏi và thành công trong sự nghiệp.
